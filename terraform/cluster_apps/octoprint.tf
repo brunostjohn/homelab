efd6d77ef46d36a2c9b19560c59e3ce2e5cf4f14
Bruno St John <brunost.john@icloud.com>
2024-08-03 18:12:57 +0000

    klipper perhaps

--- terraform/cluster_apps/octoprint.tf
+++ terraform/cluster_apps/octoprint.tf
@@ -1,74 +1,74 @@
-resource "kubernetes_namespace" "octoprint" {
-  metadata {
-    name = "octoprint"
-  }
-}
+# resource "kubernetes_namespace" "octoprint" {
+#   metadata {
+#     name = "octoprint"
+#   }
+# }
 
-resource "argocd_application" "octoprint" {
-  depends_on = [kubernetes_namespace.octoprint]
+# resource "argocd_application" "octoprint" {
+#   depends_on = [kubernetes_namespace.octoprint]
 
-  wait = true
+#   wait = true
 
-  metadata {
-    name      = "octoprint"
-    namespace = "argocd"
-  }
+#   metadata {
+#     name      = "octoprint"
+#     namespace = "argocd"
+#   }
 
-  spec {
-    source {
-      repo_url = var.homelab_repo
-      path     = "k8s/octoprint"
-    }
+#   spec {
+#     source {
+#       repo_url = var.homelab_repo
+#       path     = "k8s/octoprint"
+#     }
 
-    destination {
-      server    = "https://kubernetes.default.svc"
-      namespace = kubernetes_namespace.octoprint.metadata[0].name
-    }
+#     destination {
+#       server    = "https://kubernetes.default.svc"
+#       namespace = kubernetes_namespace.octoprint.metadata[0].name
+#     }
 
-    sync_policy {
-      automated {
-        prune     = true
-        self_heal = true
-      }
+#     sync_policy {
+#       automated {
+#         prune     = true
+#         self_heal = true
+#       }
 
-      retry {
-        limit = "5"
-        backoff {
-          duration     = "30s"
-          max_duration = "2m"
-          factor       = "2"
-        }
-      }
-    }
-  }
-}
+#       retry {
+#         limit = "5"
+#         backoff {
+#           duration     = "30s"
+#           max_duration = "2m"
+#           factor       = "2"
+#         }
+#       }
+#     }
+#   }
+# }
 
-module "octoprint_ingress" {
-  depends_on = [argocd_application.octoprint]
-  source     = "../ingress"
+# module "octoprint_ingress" {
+#   depends_on = [argocd_application.octoprint]
+#   source     = "../ingress"
 
-  hosts     = ["octoprint.local"]
-  service   = "octoprint-service"
-  namespace = kubernetes_namespace.octoprint.metadata[0].name
-  port      = 5000
-}
+#   hosts     = ["octoprint.local"]
+#   service   = "octoprint-service"
+#   namespace = kubernetes_namespace.octoprint.metadata[0].name
+#   port      = 5000
+# }
 
-module "spoolman_ingress" {
-  depends_on = [argocd_application.octoprint]
-  source     = "../ingress"
+# module "spoolman_ingress" {
+#   depends_on = [argocd_application.octoprint]
+#   source     = "../ingress"
 
-  hosts     = ["spoolman.local"]
-  service   = "spoolman-service"
-  namespace = kubernetes_namespace.octoprint.metadata[0].name
-  port      = 8000
-}
+#   hosts     = ["spoolman.local"]
+#   service   = "spoolman-service"
+#   namespace = kubernetes_namespace.octoprint.metadata[0].name
+#   port      = 8000
+# }
 
-module "mjpeg_ingress" {
-  depends_on = [argocd_application.octoprint]
-  source     = "../ingress"
+# module "mjpeg_ingress" {
+#   depends_on = [argocd_application.octoprint]
+#   source     = "../ingress"
 
-  hosts     = ["camera.octoprint.local"]
-  service   = "akri-webcam-svc"
-  namespace = kubernetes_namespace.octoprint.metadata[0].name
-  port      = 8080
-}
+#   hosts     = ["camera.octoprint.local"]
+#   service   = "akri-webcam-svc"
+#   namespace = kubernetes_namespace.octoprint.metadata[0].name
+#   port      = 8080
+# }
