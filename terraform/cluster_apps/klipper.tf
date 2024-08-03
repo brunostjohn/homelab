9834d8caea999f116155909e2e3342d112094f66
Bruno St John <brunost.john@icloud.com>
2024-08-03 18:40:43 +0000

    fix namespacing skill issue

---
+++ terraform/cluster_apps/klipper.tf
@@ -0,0 +1,44 @@
+resource "kubernetes_namespace" "klipper" {
+  metadata {
+    name = "klipper"
+  }
+}
+
+resource "argocd_application" "klipper" {
+  depends_on = [kubernetes_namespace.klipper]
+
+  wait = true
+
+  metadata {
+    name      = "klipper"
+    namespace = "argocd"
+  }
+
+  spec {
+    source {
+      repo_url = var.homelab_repo
+      path     = "k8s/klipper"
+    }
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = kubernetes_namespace.klipper.metadata[0].name
+    }
+
+    sync_policy {
+      automated {
+        prune     = true
+        self_heal = true
+      }
+
+      retry {
+        limit = "5"
+        backoff {
+          duration     = "30s"
+          max_duration = "2m"
+          factor       = "2"
+        }
+      }
+    }
+  }
+}
