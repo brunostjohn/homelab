4f48ad5ac436f988c8262ccc52d7720d7d93f06d
Bruno St. John <bruno@mlemo.app>
2024-09-14 17:20:07 +0100

    whisper tf

---
+++ terraform/cluster_apps/whisper_webservice.tf
@@ -0,0 +1,34 @@
+resource "argocd_application" "whisper_webservice" {
+  metadata {
+    name      = "whisper-webservice"
+    namespace = "argocd"
+  }
+
+  spec {
+    source {
+      repo_url = var.homelab_repo
+      path     = "k8s/whisper-webservice"
+    }
+
+    destination {
+      server    = "https://kubernetes.default.svc"
+      namespace = kubernetes_namespace.entertainment.metadata[0].name
+    }
+
+    sync_policy {
+      automated {
+        prune     = true
+        self_heal = true
+      }
+
+      retry {
+        limit = "5"
+        backoff {
+          duration     = "30s"
+          max_duration = "2m"
+          factor       = "2"
+        }
+      }
+    }
+  }
+}
