be40a146a5fe84b9c71c2082d76388b84ab32b92
Bruno St. John <bruno@mlemo.app>
2024-09-24 21:51:02 +0100

    add authorised keys

--- nix/modules/clusterNode/users.nix
+++ nix/modules/clusterNode/users.nix
@@ -6,6 +6,10 @@
     description = "Bruno St John";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [ ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEnV+roEnvzvCtDJPEOx7DoP0gPQBLWHUKfsG9twTiGv brunost.john@icloud.com"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII3q4MlXwUAQ8CJunI6yPHuAWPmdqPocrgedkOZPGy5E brunostjohn@MacBookPro.localdomain"
+    ];
   };
 
   services.getty.autologinUser = "brunostjohn";
