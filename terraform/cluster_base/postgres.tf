86a7e069c5f85e3b5994b6212b57e8d70a59bece
Bruno St. John <bruno@mlemo.app>
2024-09-24 16:54:39 +0100

    goodbye old guy

--- terraform/cluster_base/postgres.tf
+++ terraform/cluster_base/postgres.tf
@@ -171,18 +171,3 @@ resource "argocd_application" "postgres" {
     }
   }
 }
-
-module "postgres" {
-  depends_on = [kubernetes_persistent_volume_claim.postgres, kubernetes_persistent_volume_claim.postgres_backup]
-
-  source           = "../helm_deployment"
-  namespace        = kubernetes_namespace.databases.metadata[0].name
-  name             = "postgres"
-  create_namespace = false
-  create_ingress   = false
-
-  chart           = "postgresql"
-  repo_url        = "https://charts.bitnami.com/bitnami"
-  target_revision = "15.5.21"
-  values          = file("${path.module}/values/postgres.yml")
-}
