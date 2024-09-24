d77c839bbd5101f5f431d03f0dab81be24543494
Bruno St. John <bruno@mlemo.app>
2024-09-24 19:19:23 +0100

    update inventory, config

--- nix/s1/configuration.nix
+++ nix/s1/configuration.nix
@@ -26,7 +26,6 @@
     enable = true;
     role = "server";
     extraFlags = "--disable servicelb --tls-san 10.0.2.22 --tls-san control-plane.k3s.local --etcd-expose-metrics true --node-name ${config.networking.hostName}.${config.networking.domain}";
-    tokenFile = /deploy/clustertoken;
   };
 
   boot.loader.systemd-boot.enable = true;
