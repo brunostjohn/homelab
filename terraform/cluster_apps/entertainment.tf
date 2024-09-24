173381986aafbe1032243806b5d0a4e6a883e4df
Bruno St. John <bruno@mlemo.app>
2024-09-24 16:29:38 +0100

    create pvc, use the right path lol

---
+++ terraform/cluster_apps/entertainment.tf
@@ -0,0 +1,16 @@
+resource "kubernetes_persistent_volume_claim" "entertainment_content" {
+  metadata {
+    name = "entertainment-content-pvc"
+    namespace = kubernetes_namespace.entertainment.metadata[0].name
+  }
+  spec {
+    access_modes = ["ReadWriteMany"]
+    resources {
+      requests = {
+        storage = "2Ti"
+      }
+    }
+    storage_class_name = "jabberwock-nfs-csi"
+    volume_mode = "Filesystem"
+  }
+}
