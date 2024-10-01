9b7489edfe24e25a657a30b06069b3b60b11ca50
Bruno St. John <bruno@mlemo.app>
2024-10-01 22:31:53 +0100

    get certs for 2nd domain

--- terraform/cluster_base/certificates.tf
+++ terraform/cluster_base/certificates.tf
@@ -31,6 +31,37 @@ resource "kubernetes_manifest" "root_cert_prod" {
   }
 }
 
+resource "kubernetes_manifest" "second_cert_prod" {
+  depends_on = [ kubernetes_manifest.letsencrypt_prod_issuer ]
+
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "Certificate"
+    metadata = {
+      name      = "second-cert"
+      namespace = "kube-system"
+
+      annotations = {
+        "reflector.v1.k8s.emberstack.com/reflection-allowed"      = "true"
+        "reflector.v1.k8s.emberstack.com/reflection-auto-enabled" = "true"
+      }
+    }
+
+    spec = {
+      secretName = "second-prod"
+      issuerRef = {
+        name = kubernetes_manifest.letsencrypt_prod_issuer.manifest.metadata.name
+        kind = "ClusterIssuer"
+      }
+      commonName = var.second_fqdn
+      dnsNames = [
+        var.second_fqdn,
+        "*.${var.second_fqdn}",
+      ]
+    }
+  }
+}
+
 resource "kubernetes_manifest" "root_cert_staging" {
   depends_on = [kubernetes_manifest.letsencrypt_staging_issuer]
 
@@ -59,6 +90,37 @@ resource "kubernetes_manifest" "root_cert_staging" {
         "*.${var.global_fqdn}",
         "*.static.${var.global_fqdn}",
         "*.envs.${var.global_fqdn}"
+      ]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "second_cert_staging" {
+  depends_on = [ kubernetes_manifest.letsencrypt_staging_issuer ]
+
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "Certificate"
+    metadata = {
+      name      = "second-cert-staging"
+      namespace = "kube-system"
+
+      annotations = {
+        "reflector.v1.k8s.emberstack.com/reflection-allowed"      = "true"
+        "reflector.v1.k8s.emberstack.com/reflection-auto-enabled" = "true"
+      }
+    }
+
+    spec = {
+      secretName = "second-staging"
+      issuerRef = {
+        name = kubernetes_manifest.letsencrypt_staging_issuer.manifest.metadata.name
+        kind = "ClusterIssuer"
+      }
+      commonName = var.second_fqdn
+      dnsNames = [
+        var.second_fqdn,
+        "*.${var.second_fqdn}",
       ]
     }
   }
