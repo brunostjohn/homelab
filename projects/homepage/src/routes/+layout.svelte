82b0b580b5036149cd15b4077a2065b5a072e6af
Bruno St. John <bruno@mlemo.app>
2024-10-02 21:35:00 +0100

    layouts n shit

--- projects/homepage/src/routes/+layout.svelte
+++ projects/homepage/src/routes/+layout.svelte
@@ -1,5 +1,14 @@
-<script>
+<script lang="ts">
 	import "../app.css";
+	import { ModeWatcher } from "mode-watcher";
+	import type { Snippet } from "svelte";
+
+	interface Props {
+		children: Snippet;
+	}
+
+	const { children }: Props = $props();
 </script>
 
-<slot></slot>
+<ModeWatcher />
+{@render children()}
