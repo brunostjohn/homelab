22c2b747f04ac6a937aba5e67e2c3c8dcf8da527
Bruno St. John <bruno@mlemo.app>
2024-10-03 02:04:13 +0100

    make the cards a bit nicer

--- projects/homepage/src/lib/components/media/JellyfinCarouselCard.svelte
+++ projects/homepage/src/lib/components/media/JellyfinCarouselCard.svelte
@@ -2,6 +2,7 @@
 	import { Carousel, Card } from "$lib/components/ui";
 	import type { BaseItemDto } from "@jellyfin/sdk/lib/generated-client/models/base-item-dto";
 	import type { ImageType } from "@jellyfin/sdk/lib/generated-client/models/image-type";
+	import { ArrowRightIcon, Calendar, Clapperboard, Drama } from "lucide-svelte";
 
 	interface Props {
 		item: BaseItemDto;
@@ -9,7 +10,7 @@
 	}
 
 	const { item, serverPublicUrl }: Props = $props();
-	const { Name, OriginalTitle, Id, ServerId } = $derived(item);
+	const { Name, OriginalTitle, Id, ServerId, Type, CriticRating, ProductionYear } = $derived(item);
 
 	let failedImageLoad = $state(false);
 	const handleImageLoadError = () => {
@@ -35,19 +36,46 @@
 					'Backdrop'
 				)}'); background-repeat: no-repeat; background-position: center center; background-size: cover;"
 			>
-				<Card.Content class="flex h-96 items-end justify-start p-8">
-					<div>
+				<Card.Content class="flex h-96 items-end justify-start p-6">
+					<div class="">
 						{#if !failedImageLoad}
 							<img
 								src={getJellyfinImage(item.Id, "Logo")}
 								alt={Name ?? OriginalTitle}
 								onerror={handleImageLoadError}
-								class=" max-w-80"
+								class="mb-4 max-w-80"
 							/>
 						{:else}
 							<p>{Name ?? OriginalTitle}</p>
 						{/if}
+						<div class="align-center flex items-center gap-4">
+							<p class="align-center text-muted-foreground flex items-center text-sm">
+								<Calendar class="mr-1 inline-block h-4 w-4" />
+								{ProductionYear}
+							</p>
+							{#if Type === "Movie"}
+								<p class="align-center text-muted-foreground flex items-center text-sm">
+									<Clapperboard class="mr-1 inline-block h-4 w-4" />
+									Movie
+								</p>
+							{:else if Type === "Series"}
+								<p class="align-center text-muted-foreground flex items-center text-sm">
+									<Drama class="mr-1 inline-block h-4 w-4" />
+									Series
+								</p>
+							{/if}
+							<p class="align-center text-muted-foreground flex items-center text-sm">
+								<span class="mr-1 inline-block h-4 w-4">🍅</span>
+								{CriticRating}
+							</p>
+						</div>
 					</div>
+					<p
+						class="align-center text-muted-foreground ml-auto flex items-center justify-center gap-1 text-sm font-semibold uppercase"
+					>
+						Stream now
+						<ArrowRightIcon class="h-4 w-4" />
+					</p>
 				</Card.Content>
 			</Card.Root>
 		</a>
