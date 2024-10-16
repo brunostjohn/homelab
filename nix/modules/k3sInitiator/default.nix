7c686197644a9069d780ac80f76af726ac6daf45
Bruno St John <brunost.john@icloud.com>
2024-10-16 13:09:09 +0100

    registry mirror

--- nix/modules/k3sInitiator/default.nix
+++ nix/modules/k3sInitiator/default.nix
@@ -4,6 +4,6 @@
   services.k3s = {
     enable = true;
     role = "server";
-    extraFlags = "--disable servicelb --tls-san 10.0.2.22 --tls-san control-plane.k3s.local --etcd-expose-metrics true --node-name ${config.networking.hostName}.${config.networking.domain}";
+    extraFlags = "--disable servicelb --tls-san 10.0.2.22 --tls-san control-plane.k3s.local --etcd-expose-metrics true --node-name ${config.networking.hostName}.${config.networking.domain} --embedded-registry";
   };
 }
--- nix/modules/k3sMaster/default.nix
+++ nix/modules/k3sMaster/default.nix
@@ -4,7 +4,7 @@
   services.k3s = {
     enable = true;
     role = "server";
-    extraFlags = "--disable servicelb --tls-san 10.0.2.22 --tls-san control-plane.k3s.local --etcd-expose-metrics true --node-name ${config.networking.hostName}.${config.networking.domain}";
+    extraFlags = "--disable servicelb --tls-san 10.0.2.22 --tls-san control-plane.k3s.local --etcd-expose-metrics true --node-name ${config.networking.hostName}.${config.networking.domain} --embedded-registry";
     tokenFile = /deploy/clustertoken;
     serverAddr = "https://10.0.0.2:6443";
   };
