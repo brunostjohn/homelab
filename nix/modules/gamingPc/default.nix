ff926f5bb6713f0b988bf86cde96f5a6122a8c1c
Bruno St John <brunost.john@icloud.com>
2024-10-25 20:08:58 +0100

    minimal

--- nix/modules/gamingPc/default.nix
+++ nix/modules/gamingPc/default.nix
@@ -12,10 +12,9 @@
 
   services.xserver.enable = true;
 
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  services.displayManager.defaultSession = "plasma";
-  services.displayManager.sddm.wayland.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
 
   services.printing.enable = true;
 
