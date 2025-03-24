c4ffc5d1e102cd093acae0a8a6cef930b619d366
Bruno St John <121534527+brunostjohn@users.noreply.github.com>
2025-03-24 20:39:53 +0000

    rebuild homepage

--- projects/homepage/src/lib/trpc/client.ts
+++ projects/homepage/src/lib/trpc/client.ts
@@ -3,6 +3,7 @@ import type { QueryClient } from "@tanstack/svelte-query";
 import type { Router } from "./router";
 import { createTRPCClient, type TRPCClientInit } from "trpc-sveltekit";
 import { browser } from "$app/environment";
+import { httpLink } from "@trpc/client/links/httpLink";
 
 let browserClient: ReturnType<typeof svelteQueryWrapper<Router>>;
 
@@ -10,7 +11,7 @@ export function trpc(init?: TRPCClientInit, queryClient?: QueryClient) {
 	if (!browser) return;
 	if (browser && browserClient) return browserClient;
 	const client = svelteQueryWrapper<Router>({
-		client: createTRPCClient<Router>({ init }),
+		client: createTRPCClient<Router>({ links: [httpLink({ url: "/trpc" })] }),
 		queryClient,
 	});
 	if (browser) browserClient = client;
--- projects/homepage/src/routes/api/jellyfin/getImage/+server.ts
+++ projects/homepage/src/routes/api/jellyfin/getImage/+server.ts
@@ -1,10 +1,11 @@
 import { z } from "zod";
 import type { RequestHandler } from "./$types";
 import { error } from "@sveltejs/kit";
-import { Jellyfin } from "@jellyfin/sdk";
 import { env } from "$env/dynamic/private";
 import { hostname } from "os";
 import type { ImageType } from "@jellyfin/sdk/lib/generated-client/models";
+import { getImageApi } from "@jellyfin/sdk/lib/utils/api";
+import { Jellyfin } from "@jellyfin/sdk";
 
 const schema = z.object({
 	itemId: z.string(),
@@ -31,7 +32,8 @@ export const GET: RequestHandler = async ({ url: { searchParams }, setHeaders, f
 		return error(400, "Missing required parameters");
 	}
 	const { itemId: id, imageType: type } = schema.parse({ itemId, imageType });
-	const url = api.getItemImageUrl(itemId, imageType as ImageType);
+	const imageApi = await getImageApi(api);
+	const url = imageApi.getItemImageUrl({ Id: id }, type as ImageType);
 
 	if (!url) {
 		return error(404, "Image not found");
